4036705ec1c1a464078b018b7a03e78dbffbbe82
Xiaoshuai Geng <xiaoshuaigeng@gmail.com>
2021-11-19 17:12:37 +0000

    fixed /checkout, now will update item_in_stock

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -233,8 +233,27 @@ router.post("/checkout", async (req, res) => {
             // clear user shopping cart
             await models.cart.destroy({where: { user_id } })
 
+            Promise.all(
+                ordered_items.map( async(item) => {
+                    // console.log(item)
+                    await models.product.decrement({
+                        item_in_stock: item.quantity },
+                        { 
+                            where: 
+                            { 
+                                product_id: item.product_id 
+                            } 
+                        })
+                })
+            )
+
             return checkout_items
         })
+
+        // ordered_items.forEach(async (item) => {
+        //     console.log(item)
+        //     const await models.product.update({ quantity: item.quantity - 1 }, { where: { product_id: item.product_id } })
+        // })
 
         // console.log(ordered_items)
         // const created_at = order.created_at
