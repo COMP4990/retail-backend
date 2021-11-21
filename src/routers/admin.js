99886c759f8883d64c51d160b2db03e66b888896
Xiaoshuai Geng <xiaoshuaigeng@gmail.com>
2021-11-21 07:03:45 +0000

    Reformat returned results in /salesPrice, add /topSellingBrand and /topSellingProduct

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -84,38 +84,41 @@ router.get("/numOfAllOrders", async (req, res) => {
     }
 })
 
-/**
- * 
- * SELECT O.created_at as DATE, SUM(subtotal) 
- * FROM ORDER_FACT as H, DIM_ORDER as O 
- * WHERE H.order_key = O.order_key AND O.payment_status = 1 
- * GROUP BY O.created_at
- */
 
-router.get("/salesPrice/:Days", async (req, res) =>{
-    const Days = req.params.Days
+router.get("/salesPrice/:mode", async (req, res) =>{
+    const mode = req.params.mode
     try {
         var daySpan;
-        if (Days == "day") {
+        if (mode == "day") {
             daySpan = 1
         }
-        if (Days == "week") {
+        if (mode == "week") {
             daySpan = 7
         }
-        if (Days == "month") {
-            daySpan = 31
+        if (mode == "month") {
+            daySpan = 30
+        }
+
+        if (mode == "year") {
+            daySpan = 365
         }
 
         if (daySpan) {
             var result = await dw_sequelize.query(
-                'SELECT DATE(O.created_at) as `Date`, SUM(subtotal) as `Sales` ' +
-                'FROM ORDER_FACT as H, DIM_ORDER as O ' +
-                'WHERE H.order_key = O.order_key AND O.payment_status = 1 ' +
-                'GROUP BY DATE(O.created_at);',
+                'SELECT DISTINCT DIM_DATE.date as Date, IFNULL(SUM(subtotal),0) as Sales FROM DIM_DATE ' +
+                'LEFT JOIN DIM_TIME ON DIM_DATE.date = DIM_TIME.created_at ' +
+                'LEFT JOIN ORDER_FACT ON ORDER_FACT.time_key = DIM_TIME.time_key ' +
+                'LEFT JOIN DIM_ORDER ON ORDER_FACT.order_key = DIM_ORDER.order_key ' +
+                'WHERE DIM_DATE.date BETWEEN DATE_SUB(curdate(), INTERVAL :daySpan DAY) AND curdate() ' +
+                'GROUP BY DIM_DATE.date ' +
+                'ORDER by DIM_DATE.date ASC',
                 {
-                    type: QueryTypes.SELECT
-                })
+                    type: QueryTypes.SELECT,
+                    replacements: {daySpan: daySpan}
+                }
+            )
 
+            // parse decimal to float to remove the double quotation mark
             result.forEach( (item) => {
                 // console.log(item.scales)
                 item.Sales = parseFloat(item.Sales)
@@ -129,6 +132,50 @@ router.get("/salesPrice/:Days", async (req, res) =>{
         
     }
     
+})
+
+router.get("/topSellingBrand", async (req, res) => {
+    try {
+        var result = await dw_sequelize.query(
+            'SELECT brand_name as `brand`, SUM(quantity) as `quantity` FROM DIM_PRODUCT ' +
+            'INNER JOIN ORDER_FACT ON ORDER_FACT.product_key = DIM_PRODUCT.product_key ' +
+            'INNER JOIN DIM_ORDER ON ORDER_FACT.order_key = DIM_ORDER.order_key ' +
+            'GROUP BY brand_name LIMIT 5;', {
+            type: QueryTypes.SELECT
+        })
+        
+        // remove double quotation mark for value data
+        result.forEach((item) => {
+            item.quantity = parseInt(item.quantity)
+        })
+
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get("/topSellingProduct", async (req, res) => {
+    try {
+        var result = await dw_sequelize.query(
+            'SELECT product_name as `product`, SUM(quantity) as `quantity` FROM DIM_PRODUCT ' +
+            'INNER JOIN ORDER_FACT ON ORDER_FACT.product_key = DIM_PRODUCT.product_key ' +
+            'INNER JOIN DIM_ORDER ON ORDER_FACT.order_key = DIM_ORDER.order_key ' +
+            'GROUP BY product_name ' +
+            'ORDER BY quantity DESC '+ 
+            'LIMIT 5; ', {
+            type: QueryTypes.SELECT
+        })
+
+        // remove double quotation mark for value data
+        result.forEach((item) => {
+            item.quantity = parseInt(item.quantity)
+        })
+
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 module.exports = router
